bd53cb9e23219b9ebde53d862a89d6bf0de0b7e3
AshishKharel <ashishkharel58@gmail.com>
2021-03-21 13:18:49 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee ={name:'Ashish Kharel',streetAddress:'8060 Asland Ave'};
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newEmployee={...obj};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj,key,value){
+    const newEmployee={...obj}
+    delete newEmployee[key]
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(obj,key,value){
+    delete obj[key]
+    return obj;
+}
